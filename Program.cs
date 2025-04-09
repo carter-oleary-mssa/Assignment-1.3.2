d9457e28cf1c7800f20a003d7f0ff314eed8a1e4
carter-oleary <carter.m.oleary@outlook.com>
2025-04-09 13:21:24 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("----- DIFFERENT WAYS TO INITIALIZE ARRAYS ------");
+Console.WriteLine("1. Creating an empty array and adding values later");
+string[] array = new string[3];
+Console.WriteLine("Initial Array Values:");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+array[0] = "One";
+array[1] = "Two";
+array[2] = "Three";
+Console.WriteLine("Changed array values: ");
+for(int i = 0;i < array.Length;i++)
+{
+    Console.WriteLine(array[i]);
+}
+
+Console.WriteLine("\n\n2. Creating an array and assigning values immediately");
+string[] array2 = new string[3] {"Uno", "Dos", "Tres"};
+for(int i = 0;  i < array2.Length; i++)
+{
+    Console.WriteLine(array2[i]);
+}
+
+Console.WriteLine("\n\n3. Creating an array without specifying size");
+string[] noSizeArray = new string[] {"Bananas", "Bagels", "Eggs", "Salt"};
+for(int i = 0; i<noSizeArray.Length; i++)
+{
+    Console.WriteLine(noSizeArray[i]);
+}
+
+Console.WriteLine("\n\n4. Creating an array without using \'new\' keyword or specifying size");
+string[] noNewArray = { "Wow", "This", "Is", "Pretty", "Neat" };
+for (int i = 0; i < noNewArray.Length; i++)
+{
+    Console.WriteLine(noNewArray[i]);
+}
